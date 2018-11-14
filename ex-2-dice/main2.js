0622c4b355b2619ed802b096ede1995355910ac7
G3org3B <vbt.george@gmail.com>
2018-11-14 23:44:52 +0100

    fix complete ex

--- ex-2-dice/main2.js
+++ ex-2-dice/main2.js
@@ -10,23 +10,28 @@ var tiro_player2;
 
 var tiro_player1 = Math.floor(Math.random() * 7);
 console.log(Math.floor(Math.random() * 7));
+document.writeln(tiro_player1);
 
 var tiro_player2 = Math.floor(Math.random() * 7);
 console.log(Math.floor(Math.random() * 7));
+document.writeln(tiro_player2);
 
 var riprova_tiro = tiro_player1 == tiro_player2;
 
 // CONFRONTO TRA I DUE TIRI
 
-
-if (tiro_player1 > tiro_player2) {
-
-  document.writeln("il player1 ha vinto");
-}
-else {
-  document.writeln("il player2 ha vinto");
-}
+// PARITA'
 if (riprova_tiro) {
 
   document.writeln("riprovare il tiro");
+}
+// VINCITA'
+if (tiro_player1 > tiro_player2) {
+console.log();
+  document.writeln("il player1 ha vinto");
+}
+// SCONFITTA
+else {
+
+  document.writeln("il player2 ha vinto");
 }
